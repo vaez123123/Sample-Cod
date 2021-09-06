94a8533bc8bea2126796cf3dc01aeb70ece8be7e
PARINAZ VAEZ <88229843+vaez123123@users.noreply.github.com>
2021-09-06 17:00:40 +0430

    Update ObjectiveSkp.cs

--- Constraints and Objectives Functions/ObjectiveSkp.cs
+++ Constraints and Objectives Functions/ObjectiveSkp.cs
@@ -9,14 +9,15 @@ namespace SKPScheduling
 {
     public class ObjectiveSkp : ObjectiveL2
     {
-        // تابع محاسبه مقدار تابع هدف
+        // Calculate the value of the objective function, between zero and one
+        
         //ALL
         public override void calcuTotalObj(Solution solu, int idEfrazLocal, int idSarfaslLocal, CommonLists Lst, ref int doubleChekWorkRoll,
             List<ChangeRoll> ChangeRolls, ref bool chekChangeCountProg, ref int chekChangRoll, ref int reasonWorkRoll)
         {
 
 
-            calcuObjRoll(ref doubleChekWorkRoll,Lst.currSolution,Lst.RollsWork);// بین صفر و یک
+            calcuObjRoll(ref doubleChekWorkRoll,Lst.currSolution,Lst.RollsWork);//  
 
             calcuObjContinuePattern(Lst);
 
@@ -36,8 +37,8 @@ namespace SKPScheduling
             {
                 int chek = chekChangeWorkRoll(RollsWork, ref doubleChekWorkRoll,currSolution);
                 currSolution.ChekChangeWorkRoll = chek;
-                // غیر سر فصل
 
+                // is not sarfasl
                 if (chek == 30 || chek == 31)
                 {
 
@@ -64,7 +65,7 @@ namespace SKPScheduling
                             currSolution.ObjWeiWorkRoll = 0;
                     }
                 }
-                // سرفصل
+                // sarfasl
                 else
                 {
 
@@ -125,8 +126,6 @@ namespace SKPScheduling
             {
                 int chek = chekChangeWorkRoll(RollsWork,ref doubleChekWorkRoll,currSolution);
                 currSolution.ChekChangeWorkRoll = chek;
-
-
                 if (RollsWork.Last().LenOpt != 0)
                 {
                     double objNew = 0;
@@ -137,8 +136,6 @@ namespace SKPScheduling
 
                     else
                         objNew = 0;
-
-
                     double obj = 0;
                     double dif = RollsWork.Last().LenOpt - RollsWork.Last().LenDB - RollsWork.Last().LenRelease -
                                    RollsWork.Last().CurrentTotalFixLen;
@@ -150,7 +147,6 @@ namespace SKPScheduling
                         obj = 0;
 
                     currSolution.ObjLenWorkRoll = obj * DataBase.CoefObjWorkRollOld + objNew * DataBase.CoefObjWorkRollNew;
-
 
                 }
                 if (RollsWork.Last().WeiOpt != 0)
@@ -178,30 +174,20 @@ namespace SKPScheduling
                 }
 
             }
-
-
-
-
         }
 
          //TEM-SKP1
         public int chekChangeWorkRoll(List<Roll> lstRollLocal,ref int  doubleChekWorkRoll,Solution currSolution)
-        {
-            
+        {         
             #region if (lstRollLocal.Last().weiOpt != 0)
 
             if (lstRollLocal.Last().WeiOpt != 0)
             {
-                // تغییر غلتک به دلیل سرفصل
-
+                // change roll due to sarfasl
                 if (Status.IndexSarfasl != -1 && Status.IndexSarfasl !=currSolution.IndexSarfasl)
                 {
-                    //تغییر غلتک به دلیل ظرفیت
-
-                    // فقط با مقدار بهینه مقایسه می شود زیرا  به دلیل سرفصل غلتک عوض شده است بنابراین
-                    // غلتک جدید دیگر مقدار فیکس یا ریلیز یا دی بی ندارد
-
-                    if ((lstRollLocal.Last().WeiOpt) * (1-lstRollLocal.Last().LowerPerc) < (//lstRollLocal.Last().weiDB
+                   // change roll due to capacit or Progarm type is "MB"
+                   if ((lstRollLocal.Last().WeiOpt) * (1-lstRollLocal.Last().LowerPerc) < (//lstRollLocal.Last().weiDB
                         //  + lstRollLocal.Last().currentTotalFixWei
                         // + lstRollLocal.Last().weiRelease
                                                            +currSolution.WeiProg))
@@ -210,23 +196,17 @@ namespace SKPScheduling
                     else
                         return 11;
                 }
-
-
                 else
                 {
                     if (doubleChekWorkRoll == 1)
                     {
-                        //تغییر غلتک به دلیل ظرفیت
-
-
+                          // change roll due to capacit or Progarm type is "MB"
                         if ((lstRollLocal.Last().WeiOpt) * (1 - lstRollLocal.Last().LowerPerc) < (lstRollLocal.Last().WeiDB
                                                                 + lstRollLocal.Last().CurrentTotalFixWei
                                                                 + lstRollLocal.Last().WeiRelease
                                                                 + currSolution.WeiProg))
 
                             return 30;
-
-
                         else
                             return 31;
 
@@ -235,46 +215,30 @@ namespace SKPScheduling
                     //(doubleChekWorkRoll == 2)
                     else
                     {
-                        // فقط با مقدار بهینه مقایسه می شود زیرا  به دلیل غلتک جدید یعنی
-                        //(doubleChekWorkRoll == 2)
-                        //غلتک عوض شده است بنابراین
-                        // غلتک جدید دیگر مقدار فیکس یا ریلیز یا دی بی ندارد
+                          // change roll due to capacit
 
                         if ((lstRollLocal.Last().WeiOpt) * (1 - lstRollLocal.Last().LowerPerc) < (//lstRollLocal.Last().weiDB
                             //  + lstRollLocal.Last().currentTotalFixWei
                             // + lstRollLocal.Last().weiRelease
                                                                +currSolution.WeiProg))
                             return 20;
-
-
                         else
                             return 21;
-
-
                     }
 
                 }
-
-
-
             }
             #endregion
-
 
             #region if (lstRollLocal.Last().lenOpt != 0)
 
             else
             {
-
-                // تغییر غلتک به دلیل سرفصل
-
+                // Roller change due to sarfasl
                 if (Status.IndexSarfasl != -1 && Status.IndexSarfasl != currSolution.IndexSarfasl)
                 {
-                    //تغییر غلتک به دلیل ظرفیت
 
-                    // فقط با مقدار بهینه مقایسه می شود زیرا  به دلیل سرفصل غلتک عوض شده است بنابراین
-                    // غلتک جدید دیگر مقدار فیکس یا ریلیز یا دی بی ندارد
-
+                       // change roll due to capacity
                     if ((lstRollLocal.Last().LenOpt) * (1 - lstRollLocal.Last().LowerPerc) < (//lstRollLocal.Last().lenDB
                         //  + lstRollLocal.Last().currentTotalFixLen
                         // + lstRollLocal.Last().lenRelease
@@ -288,16 +252,13 @@ namespace SKPScheduling
                 {
                     if (doubleChekWorkRoll == 1)
                     {
-                        //تغییر غلتک به دلیل ظرفیت
-
-
+                          // change roll due to capacity
                         if ((lstRollLocal.Last().LenOpt) * (1 - lstRollLocal.Last().LowerPerc) < (lstRollLocal.Last().LenDB
                                                                 + lstRollLocal.Last().CurrentTotalFixLen
                                                                 + lstRollLocal.Last().LenRelease
                                                                 + currSolution.LenProg))
 
                             return 30;
-
 
                         else
                             return 31;
@@ -307,11 +268,7 @@ namespace SKPScheduling
                     //(doubleChekWorkRoll == 2)
                     else
                     {
-                        // فقط با مقدار بهینه مقایسه می شود زیرا  به دلیل غلتک جدید یعنی
-                        //(doubleChekWorkRoll == 2)
-                        //غلتک عوض شده است بنابراین
-                        // غلتک جدید دیگر مقدار فیکس یا ریلیز یا دی بی ندارد
-
+          
                         if ((lstRollLocal.Last().LenOpt) * (1 - lstRollLocal.Last().LowerPerc) < (//lstRollLocal.Last().lenDB
                             //  + lstRollLocal.Last().currentTotalFixLen
                             // + lstRollLocal.Last().lenRelease
@@ -322,18 +279,11 @@ namespace SKPScheduling
                         else
                             return 21;
 
-
                     }
 
                 }
-
-
-
-
             }
             #endregion
-
-
         }
 
 
